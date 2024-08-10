13626ab9b290b65ffc796dae8e17a6b37d575933
Alex <alex.safonovaa@yandex.ru>
2024-08-10 23:41:12 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,12 @@
+first=int(input("Введите 1 число: "))
+second =int(input("Введите 2 число: "))
+third =int(input("Введите 3 число: "))
+if first==second==third:
+    print("3")
+elif first==second and first!=third or first==third and second!=third or second==third and first!=third:
+    print("2")
+elif first!=second!=third:
+    print("0")
+
+my_list=[42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+while 
