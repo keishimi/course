cda34833b98a65660ea178a446841a1fc171d128
Alex <alex.safonovaa@yandex.ru>
2024-10-28 18:43:25 +0300

    Add files via upload

---
+++ module_14_5.py
@@ -0,0 +1,154 @@
+import texts
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import asyncio
+from crud_functions import *
+
+api = ''
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+button1 = KeyboardButton(text='Рассчитать')
+button2 = KeyboardButton(text='Информация')
+button3 = KeyboardButton(text='Купить')
+button4 = KeyboardButton(text='Регистрация')
+kb.row(button1, button2, button3, button4)
+
+kb_inl = InlineKeyboardMarkup(resize_keyboard=True)
+button_in1 = InlineKeyboardButton(text='Рассчитать норму калорий', callback_data='calories')
+button_in2 = InlineKeyboardButton(text='Формулы расчёта', callback_data='formulas')
+kb_inl.add(button_in1)
+kb_inl.add(button_in2)
+
+product_inl = InlineKeyboardMarkup(resize_keyboard=True)
+p1 = InlineKeyboardButton(text='roduct1', callback_data='product_buying')
+p2 = InlineKeyboardButton(text='roduct2', callback_data='product_buying')
+p3 = InlineKeyboardButton(text='roduct3', callback_data='product_buying')
+p4 = InlineKeyboardButton(text='roduct4', callback_data='product_buying')
+product_inl.add(p1, p2)
+product_inl.add(p3, p4)
+
+
+class RegistrationState(StatesGroup):
+    username = State()
+    email = State()
+    age = State()
+    balance = State()
+
+
+@dp.message_handler(text='Регистрация')
+async def sing_up(message):
+    await message.answer('Введите имя пользователя (только латинский алфавит):')
+    await RegistrationState.username.set()
+
+
+@dp.message_handler(state=RegistrationState.username)
+async def set_username(message, state):
+    if is_included(message.text):
+        await message.answer('Пользователь существует, введите другое имя')
+        await RegistrationState.username.set()
+    else:
+        await state.update_data(username=message.text)
+        await message.answer('Введите свой email:')
+        await RegistrationState.email.set()
+
+
+@dp.message_handler(state=RegistrationState.email)
+async def set_email(message, state):
+    await state.update_data(email=message.text)
+    await message.answer('Введите свой возраст:')
+    await RegistrationState.age.set()
+
+
+@dp.message_handler(state=RegistrationState.age)
+async def set_age_registration(message, state):
+    await state.update_data(age=message.text)
+    users_data = await state.get_data()
+    add_user(username=users_data["username"], email=users_data["email"], age=int(users_data["age"]))
+    await state.finish()
+    await message.answer("Регистрация прошла успешно")
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(text='Рассчитать')
+async def main_menu(message):
+    await message.answer('Выберите опцию:', reply_markup=kb_inl)
+
+
+@dp.callback_query_handler(text='formulas')
+async def get_formulas(call):
+    await call.message.answer('10 х вес (кг) + 6,25 x рост (см) – 5 х возраст (г) + 5')
+
+
+@dp.callback_query_handler(text='calories')
+async def set_age(call):
+    await call.message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    await message.answer('Введите свой рост:')
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=message.text)
+    await message.answer('Введите свой вес:')
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    calories = 10 * int(data['weight']) + 6.25 * int(data['growth']) - 5 * int(data['age']) + 5
+    await message.answer(f'Ваша норма коллорий: {calories}')
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+    await message.answer('Привет! Я бот помогающий твоему здоровью.', reply_markup=kb)
+
+
+initiate_db()
+products = get_all_products()
+
+
+@dp.message_handler(text='Купить')
+async def get_buying_list(message):
+    for product in products:
+        id, title, description, price = product
+        await message.answer(f'Название: {title} | Описание: {description} | Цена: {price}')
+        with open(f'/Users/miron/PycharmProjects/Urban/UrbanProject/output/module14/{id}.png', "rb") as img:
+            await message.answer_photo(img)
+    await message.answer('Выберите продукт для покупки:', reply_markup=product_inl)
+
+
+@dp.callback_query_handler(text='product_buying')
+async def send_confirm_message(call):
+    await call.message.answer('Вы успешно приобрели продукт!')
+    await call.answer()
+
+
+@dp.message_handler()
+async def all_message(message):
+    print('Введите команду /start, чтобы начать общение.')
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
