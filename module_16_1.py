b25820fe59a7b2ec60d5e87927998c89c8744d39
Alex <alex.safonovaa@yandex.ru>
2024-12-05 13:58:54 +0300

    Add files via upload

--- module_16_1.py
+++ module_16_1.py
@@ -2,18 +2,22 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
-async def welcome():
+async def root():
     return {"message": "Главная страница"}
 
+
 @app.get("/user/admin")
-async def root():
+async def admin():
     return {"message": "Вы вошли как администратор"}
+
+
 @app.get("/user/{user_id}")
-async def welcome(user_id: int):
-    return {"message": "Вы вошли как пользователь № {user_id}"}
-@app.get("/user/{username}/{age}")
-async def welcome(username: str, age:int):
-    return {"message": "Информация о пользователе. Имя: {username}, Возраст: {age}"}
+async def user(user_id: int):
+    return {"message": f"Вы вошли как пользователь № {user_id}"}
 
 
+@app.get("/user")
+async def user_info(username: str, age: int):
+    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
